4359b4922e5a7ff5831ed8a8beccd41ce6844f91
Michal Hloch <hloch.michal@gmail.com>
2025-12-06 22:37:54 +0100

    .

--- src-tauri/src/db.rs
+++ src-tauri/src/db.rs
@@ -18,11 +18,31 @@ pub async fn init_db() -> Result<Pool<Sqlite>, sqlx::Error> {
                 process_name TEXT NOT NULL,
                 window_title TEXT NOT NULL,
                 start_time TEXT NOT NULL,
+                temp_end_time TEXT NOT NULL,
                 end_time TEXT
                 )",
     ).execute(&pool).await?;
 
     Ok(pool)
+}
+/// Intended as a failsafe in case of sudden (and, in fact, any) shutdowns by periodically storing time in temp value
+// this can then be used as an end time, if it doesn't exist
+///
+/// # Arguments
+///
+/// * `pool`: the connection to a SQLite database
+///
+/// returns: Result<bool, Error>
+///
+pub async fn sync_fallback(pool: &Pool<Sqlite>) -> Result<bool, sqlx::Error> {
+    let now = chrono::Local::now().to_rfc3339();
+
+    sqlx::query("UPDATE activity_log SET temp_end_time = ? WHERE end_time IS NULL")
+        .bind(now)
+        .execute(pool)
+        .await?;
+
+    Ok(true)
 }
 
 pub async fn log_switch(pool: &Pool<Sqlite>, process_name: &str, title: &str) -> Result<(), sqlx::Error> {
