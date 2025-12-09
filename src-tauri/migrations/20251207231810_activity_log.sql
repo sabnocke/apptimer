a2dd8e90ae03a891c364f6857777fcc63a81611d
Michal Hloch <hloch.michal@gmail.com>
2025-12-09 08:52:53 +0100

    delete

--- src-tauri/migrations/20251207231810_activity_log.sql
+++
@@ -1,9 +0,0 @@
--- Add migration script here
-CREATE TABLE IF NOT EXISTS activity_log (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    process_name TEXT NOT NULL,
-    window_title TEXT NOT NULL,
-    start_time DATETIME NOT NULL,
-    temp_end_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    end_time DATETIME
-);
